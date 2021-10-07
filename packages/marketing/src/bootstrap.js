08d6a4da9a0ca8b365d3bd07ca1f651580d59fcb
Rodrigo Guerrero <rodrigoh00per1@gmail.com>
2021-10-07 09:50:12 -0500

    add space for taking the changes

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,7 +4,6 @@ import App from "./components/App";
 
 const mount = (el) => ReactDOM.render(<App />, el)
 
-
 if (process.env.NODE_ENV === "development") {
     const devRoot = document.querySelector("#_marketing-dev-root")
     if (devRoot)
