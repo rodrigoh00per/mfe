cd14ade260d22907d5ab29a82da597bf8160c1c5
Rodrigo Guerrero <rodrigoh00per1@gmail.com>
2021-10-13 22:04:22 -0500

    change of container styles

--- packages/container/src/components/App.js
+++ packages/container/src/components/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 import MarketingApp from './MarketingApp';
 import Header from "./Header";
 import { BrowserRouter } from 'react-router-dom';
-
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 const App = () =>
 (<BrowserRouter>
-container
-    <div>
-        <Header />
-        <MarketingApp />
-    </div>
+    <Header />
+    <MarketingApp />
+
 </BrowserRouter>
 );
 
